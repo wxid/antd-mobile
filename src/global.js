417ba28cec6925bf16d7ae115c8bd7a8abbe4774
JiangHao <978561120@qq.com>
2019-05-27 08:45:42 +0800

    路由跳转更改

--- src/global.js
+++ src/global.js
@@ -1,2 +1,22 @@
 // 这里引用全局js
 import '@babel/polyfill';
+import request from '@/services/request';
+
+request('https://wx.haplox.cn/api/dingding/getconfig/sales').then(
+  (res) => {
+    let _config = res;
+    dd.config({
+      agentId: _config.agentId,
+      corpId: _config.corpId,
+      timeStamp: _config.timeStamp,
+      nonceStr: _config.nonceStr,
+      signature: _config.signature,
+      jsApiList: ['runtime.info', 'biz.util.scan', 'biz.util.uploadImage', 'biz.util.previewImage'],
+    });
+  },
+  () => {
+    alert('ddconfig 请求失败');
+  }
+)
+
+console.log(145145)
--- src/models/global.js
+++ src/models/global.js
@@ -1,6 +1,7 @@
 // import { queryNotices } from '@/services/api';
 // import { isWechat, getClearPath, getClearHref, getClearSearch, loginWechat, getQueryString } from '@/config/login';
-import {} from '@/services/module/user'
+import { me, ddLogin } from '@/services/module/user';
+import { getAuthCode } from '@/config/dd';
 import router from 'umi/router';
 
 export default {
@@ -12,14 +13,50 @@ export default {
   },
 
   effects: {
-    * getUser({}, { call, put, select }) {
+    * getUser({ payload }, { call, put, select }) {
       console.log(4321)
+      dd.ready(function() {
+        let _corpId = 'dingf77248eb1d8e040b35c2f4657eb6378f' // 企业在钉钉内的唯一标识
+        dd.device.notification.showPreloader({ // 显示正在登录 loading 动画
+          text: "登录中..", // loading 显示的字符，空表示不显示文字
+          showIcon: true, // 是否显示icon，默认true
+          onSuccess: function(result) {},
+          onFail: function(err) {}
+        })
+        dd.runtime.permission.requestAuthCode({
+          corpId: _corpId,
+          onSuccess: (info) => {
+            console.log('authcode: ' + info.code) // 通过该免登授权码可以换取用户身份
+            // yield put({
+            //   type: 'clearNotices',
+            //   payload: {
+            //     code: info.code,
+            //   },
+            // });
+            // const response = yield call(ddLogin, { code: info.code }); // 使用 authCode 请求登录
+            // if (response) {
+            //   dd.device.notification.hidePreloader({ // 关闭正在登录 loading 动画
+            //     onSuccess: function(result) {
+            //       router.push(payload.path);
+            //     },
+            //     onFail: function(err) {}
+            //   })
+            // }
+          },
+          onFail: function(err) {
+            console.log('requestAuthCode fail: ' + JSON.stringify(err))
+          }
+        })
+      })
     },
     * getMe({}, { call, put, select }) {
 
     },
-    * clearNotices({ payload }, { put, select }) {
+    * getAuthCode({}, { call, put, select }) {
 
+    },
+    * clearNotices({ payload }, { put, select }) {
+      console.log(111111)
     },
     * changeNoticeReadState({ payload }, { put, select }) {
 
@@ -54,12 +91,18 @@ export default {
         if (token) {
           // 使用 jwt-token 请求登录
           dispatch({
-            type: 'getUser',
+            type: 'getMe',
           });
         } else if (process.env.NODE_ENV === 'development') {
           // 本地登录
+          dispatch({
+            type: 'getUser'
+          });
         } else {
           // 获取免登授权码
+          dispatch({
+            type: 'getAuthCode'
+          });
         }
       });
     },
